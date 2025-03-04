c4ebbc873c5d2bbf1357d1609c393afb101205bc
Madhvi N <madhvinandanwar@gmail.com>
2025-03-04 16:55:02 +0530

    Updated settings

--- snaplink/settings.py
+++ snaplink/settings.py
@@ -138,7 +138,7 @@ SITE_ID = 1
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
